f907bea974455be039085b933d9b3114677f3fef
AnatolSher <maxlab@inbox.ru>
2024-05-20 11:19:55 +0300

    test_adc_all updated, noniso func added

--- libraries/Basic/examples/test_adc_all/test_adc_all.ino
+++ libraries/Basic/examples/test_adc_all/test_adc_all.ino
@@ -6,6 +6,7 @@ void setup()
 	pinMode(PA2,ANALOG_INPUT);
 	pinMode(PA3,ANALOG_INPUT);
 	pinMode(PA4,ANALOG_INPUT);
+  Serial.begin(115200);
 }
 
 void loop()
@@ -15,12 +16,18 @@ ch1 = analogRead(PA1);
 ch2 = analogRead(PA2);
 ch3 = analogRead(PA3);
 ch4 = analogRead(PA4);
-
-printf("chan:%d  %d(mV) or %f(V)\r\n", 1, ch1, (double)ch1/1000);
-printf("chan:%d  %d(mV) or %f(V)\r\n", 2, ch2, (double)ch2/1000);
-printf("chan:%d  %d(mV) or %f(V)\r\n", 3, ch3, (double)ch3/1000);
-printf("chan:%d  %d(mV) or %f(V)\r\n", 4, ch4, (double)ch4/1000);
-
+Serial.println("=================================");
+String output_str = "";
+output_str += String("Chan:")+String(1)+String("  ")+String(ch1)+String(" (mV) or ")+String((double)ch1/1000)+String(" (V)");
+Serial.println(output_str);
+output_str = "";
+output_str += String("Chan:")+String(2)+String("  ")+String(ch2)+String(" (mV) or ")+String((double)ch2/1000)+String(" (V)");
+Serial.println(output_str);
+output_str = "";
+output_str += String("Chan:")+String(3)+String("  ")+String(ch3)+String(" (mV) or ")+String((double)ch3/1000)+String(" (V)");
+Serial.println(output_str);
+output_str = "";
+output_str += String("Chan:")+String(4)+String("  ")+String(ch4)+String(" (mV) or ")+String((double)ch4/1000)+String(" (V)");
+Serial.println(output_str);
 delay(1000);
-
 }
